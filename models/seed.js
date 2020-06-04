69b456ffd0a642f2b221112a2a004e5c02f49f3b
Tim <tsclay9@gmail.com>
2020-06-04 14:53:06 -0400

    added more seed

--- models/seed.js
+++ models/seed.js
@@ -25,5 +25,46 @@ module.exports = [
       roomNumber: '5504'
     },
     comments: 'Smoked in the room; ripped out smoke detector'
+  },
+  {
+    reservationID: '17459174',
+    name: {
+      lastName: 'Klein',
+      firstName: 'Amos'
+    },
+    recentStay: '2019-06-01',
+    accomodation: {
+      property: 'Timberline',
+      roomNumber: '4700'
+    },
+    comments:
+      'Called constantly complaining of noise. Security visited 3 times. No noise at each instance.'
+  },
+  {
+    reservationID: '17459174',
+    name: {
+      lastName: 'Price',
+      firstName: 'Patrick'
+    },
+    recentStay: '2018-06-01',
+    accomodation: {
+      property: 'Timberline',
+      roomNumber: '4700'
+    },
+    comments: 'Busted hole in wall'
+  },
+  {
+    reservationID: '17459174',
+    name: {
+      lastName: 'Jones',
+      firstName: 'Kevin'
+    },
+    recentStay: '2020-01-01',
+    accomodation: {
+      property: 'Timberline',
+      roomNumber: '4777'
+    },
+    comments:
+      'Called constantly complaining of noise. Security visited 3 times. No noise at each instance.'
   }
 ]
