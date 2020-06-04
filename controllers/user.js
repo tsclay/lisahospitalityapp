a5af69ee6378a69235f48bcd00e833015c2cac97
Tim Clay <tsclay9@gmail.com>
2020-06-04 19:27:40 -0400

    created views for login and register

--- controllers/user.js
+++ controllers/user.js
@@ -5,11 +5,11 @@ const User = require('../models/User')
 const user = express.Router()
 
 user.get('/register', (req, res) => {
-  res.render('users/register.ejs')
+  res.render('users/register.ejs', { navOn: false })
 })
 
 user.get('/login', (req, res) => {
-  res.render('users/login.ejs')
+  res.render('users/login.ejs', { navOn: false })
 })
 
 user.post('/register', (req, res) => {
