9f9a7186abc43188056eff2fdc10b6ef2e596c70
Tim <tsclay9@gmail.com>
2020-06-04 14:47:43 -0400

    refactored

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 $(() => {
   $('option').on('click', (event) => {
-    const selected = $(event.currentTarget)
+    const selectedOption = $(event.currentTarget)
 
-    $('input[type=search]').attr('placeholder', selected.text())
-    $('input[type=search]').attr('name', selected.val())
+    $('input[type=search]').attr('placeholder', selectedOption.text())
+    $('input[type=search]').attr('name', selectedOption.val())
   })
 })
