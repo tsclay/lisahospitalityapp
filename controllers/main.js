ed3c2b997e0322f7efbbccdb94f6dfb8123c946b
Tim Clay <tsclay9@gmail.com>
2020-06-05 02:26:43 -0400

    fixed seed route redirect

--- controllers/main.js
+++ controllers/main.js
@@ -13,7 +13,7 @@ const main = express.Router()
 
 main.get('/seed', (req, res) => {
   Guest.insertMany(seed, (error, addedSeed) => {
-    return error ? res.send(error) : res.redirect('app/index.ejs')
+    return error ? res.send(error) : res.redirect('/login')
   })
 })
 
