886b8766e27521a204be8e5b9327cd9df16117dc
Tim Clay <tsclay9@gmail.com>
2020-06-04 22:43:22 -0400

    deleted username; opting for first and last names

--- models/user.js
+++ models/user.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
+  name: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
+  },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 })
