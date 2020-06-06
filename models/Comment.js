cf63110d4193bc355231fa0700dd4c19e0aa6535
Tim Clay <tsclay9@gmail.com>
2020-06-05 20:46:39 -0400

    modified commenter prop

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const commentSchema = new Schema({
-  commenter: String,
+  commenter: {
+    name: String,
+    ID: Number
+  },
   body: { type: String, required: true },
   createdAt: { type: Date, default: Date.now.toLocaleString },
   edited: { type: Boolean, default: false },
