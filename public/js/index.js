bce6e135b621a354b154f880d3231bfcb36b8586
Tim Clay <tsclay9@gmail.com>
2020-06-05 22:58:30 -0400

    add styling and query selecting for button options for each entry

--- public/js/index.js
+++ public/js/index.js
@@ -5,4 +5,13 @@ $(() => {
     $('input[type=search]').attr('placeholder', selectedOption.text())
     $('input[type=search]').attr('name', selectedOption.val())
   })
+
+  $('.list-group-item').click((event) => {
+    if ($(event.currentTarget).children('.collapse').hasClass('show')) {
+      $(event.currentTarget).children('.collapse').collapse('hide')
+    } else {
+      $(event.currentTarget).siblings().children('.collapse').collapse('hide')
+      $(event.currentTarget).children('.collapse').collapse('show')
+    }
+  })
 })
