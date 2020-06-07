738c217b1d8ca7a6831733705bc80d081785fb4d
Tim <tsclay9@gmail.com>
2020-06-07 19:52:50 -0400

    css triggers on clicks

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
 $(() => {
+  $('.navbar-toggler').click(() => {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+
   $('option').on('click', (event) => {
     const selectedOption = $(event.currentTarget)
 
@@ -9,9 +13,17 @@ $(() => {
   $('.list-group-item').click((event) => {
     if ($(event.currentTarget).children('.collapse').hasClass('show')) {
       $(event.currentTarget).children('.collapse').collapse('hide')
+      $(event.currentTarget).css({
+        'background-color': '',
+        color: 'black'
+      })
     } else {
       $(event.currentTarget).siblings().children('.collapse').collapse('hide')
       $(event.currentTarget).children('.collapse').collapse('show')
+      $(event.currentTarget).css({
+        'background-color': '#4d78a5',
+        color: '#ffff'
+      })
     }
   })
 })
