85b34ad3cdd96192e2f4b964ec04790b445150b9
Tim <tsclay9@gmail.com>
2020-06-08 17:24:39 -0400

    refactored list-item toggle

--- public/js/index.js
+++ public/js/index.js
@@ -20,6 +20,12 @@ $(() => {
 
   // Toggle selected entries and unfold options to view, edit, and delete
   $('.list-group-item').click((event) => {
+    $(event.currentTarget).siblings().children('.collapse').collapse('hide')
+    $(event.currentTarget).siblings().css({
+      'background-color': '',
+      color: 'black'
+    })
+
     if ($(event.currentTarget).children('.collapse').hasClass('show')) {
       $(event.currentTarget).children('.collapse').collapse('hide')
       $(event.currentTarget).css({
@@ -27,11 +33,6 @@ $(() => {
         color: 'black'
       })
     } else {
-      $(event.currentTarget).siblings().children('.collapse').collapse('hide')
-      $(event.currentTarget).siblings().css({
-        'background-color': '',
-        color: 'black'
-      })
       $(event.currentTarget).children('.collapse').collapse('show')
       $(event.currentTarget).css({
         'background-color': '#4d78a5',
