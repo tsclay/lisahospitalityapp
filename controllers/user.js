b0088c4c907be50823e6b0c28b33f7fa36756132
Tim <tsclay9@gmail.com>
2020-06-10 00:26:51 -0400

    error msg for email already in use

--- controllers/user.js
+++ controllers/user.js
@@ -12,54 +12,88 @@ user.post('/', (req, res) => {
   // Validate inputs
   const { name, email, password, confirmPassword } = req.body
   const errors = []
+  let doesUserExist = false
 
-  // Check if all fields filled
-  if (
-    !name.firstName ||
-    !name.lastName ||
-    !email ||
-    !password ||
-    !confirmPassword
-  ) {
-    errors.push({ name: 'FillError', msg: 'Please fill in all fields.' })
-  }
+  // Check if email already exists in the DB
+  User.findOne({ email }, (error, userFound) => {
+    if (userFound) {
+      console.log('Caught it!')
+      errors.push({
+        name: 'UserAlreadyExistsError',
+        msg: 'This email already exists.'
+      })
+      res.render('users/register.ejs', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+        navOn: false
+      })
+      doesUserExist = true
+    }
+    // If email already exists, other errors don't matter
+    if (doesUserExist) {
+      return null
+    }
 
-  // Check password length
-  if (password.length < 6) {
-    errors.push({
-      name: 'PasswordLengthError',
-      msg: 'Password should have at least 6 characters.'
-    })
-  }
+    //==================================================
+    // If email is unique to our DB, continue
 
-  // Check that passwords match
-  if (password !== confirmPassword) {
-    errors.push({ name: 'PasswordMatchError', msg: 'Passwords should match.' })
-  }
+    // Check if all fields are filled
+    if (
+      !name.firstName ||
+      !name.lastName ||
+      !email ||
+      !password ||
+      !confirmPassword
+    ) {
+      errors.push({ name: 'FillError', msg: 'Please fill in all fields.' })
+    }
 
-  if (errors.length > 0) {
-    res.render('users/register.ejs', {
-      errors,
-      name,
-      email,
-      password,
-      confirmPassword,
-      navOn: false
-    })
-  } else {
-    req.body.password = bcrypt.hashSync(
-      req.body.password,
-      bcrypt.genSaltSync(10)
-    )
-    User.create(req.body, (error, newUser) => {
-      return error
-        ? console.log(error)
-        : res.render('users/login.ejs', {
-            regSuccess: newUser.name.firstName,
-            navOn: false
-          })
-    })
-  }
+    // Check password length
+    if (password.length < 6) {
+      errors.push({
+        name: 'PasswordLengthError',
+        msg: 'Password should have at least 6 characters.'
+      })
+    }
+
+    // Check that passwords match
+    if (password !== confirmPassword) {
+      errors.push({
+        name: 'PasswordMatchError',
+        msg: 'Passwords should match.'
+      })
+    }
+
+    // Re-render the page with the error messages if any
+    // else, make the user account and redirect to login
+    if (errors.length > 0) {
+      res.render('users/register.ejs', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+        navOn: false
+      })
+    } else {
+      req.body.password = bcrypt.hashSync(
+        req.body.password,
+        bcrypt.genSaltSync(10)
+      )
+      User.create(req.body, (error, newUser) => {
+        return error
+          ? console.log(error)
+          : res.render('users/login.ejs', {
+              regSuccess: newUser.name.firstName,
+              navOn: false
+            })
+      })
+    }
+    return null
+  })
 })
 
 module.exports = user
